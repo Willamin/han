5a0eab6119165e4758179dce0f77d70641203d14
Will Lewis <will@wflewis.com>
2019-08-01 21:45:22 -0400

    Make the QR code also a link

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
 	before_action :require_login, except: :home
 
 	def home
-		uri = totp.provisioning_uri("han@han.com")
-		qrcode = RQRCode::QRCode.new(uri)
+		@totp_uri = totp.provisioning_uri("han@han.com")
+		qrcode = RQRCode::QRCode.new(@totp_uri)
 		@svg = qrcode.as_svg(module_size: 4).html_safe
 	end
 
