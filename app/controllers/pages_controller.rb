6ca51a7e03ee87e6ae85f6cfe0f83269a30a6366
Will Lewis <will@wflewis.com>
2019-08-01 21:24:14 -0400

    Add svg rendering of provisioning uri

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
 	before_action :require_login, except: :home
 
-	def home; end
+	def home
+		uri = totp.provisioning_uri("han@han.com")
+		qrcode = RQRCode::QRCode.new(uri)
+		@svg = qrcode.as_svg(module_size: 4).html_safe
+	end
+
 	def secret_test; end
 end
