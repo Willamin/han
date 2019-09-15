c89bde7f95b20fdee4e8f4da007c62b5eb92532b
Will Lewis <will@wflewis.com>
2019-09-15 16:48:32 -0500

    List projects in the same order all the time

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
 	before_action :require_login
 
 	def index
-		@projects = Project.all
+		@projects = Project.order(:created_at)
 	end
 
 	def create
